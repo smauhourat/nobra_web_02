3c79f58cfd9519362734d2d2699eb77e1cd491b6
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2026-06-03 12:08:07 -0300

    fix logo blanco 3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
         <Link className="nb-brand" href="/" aria-label="Inicio">
           <img
             className="nb-brand-mark"
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/${onDark ? 'logo-blanco-2.svg' : 'logo-marca-color-4.svg'}`}
+            src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/${onDark ? 'logo-blanco-3.svg' : 'logo-marca-color-4.svg'}`}
             alt=""
           />
           <div className="nb-brand-mark-text">
