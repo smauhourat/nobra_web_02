e074ac17cae6b1a2d22316824645b19a4cf297ba
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2026-06-04 18:55:14 -0300

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
@@ -20,6 +21,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="stylesheet"
         />
       </head>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-K25P3PZ615" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-K25P3PZ615');
+        `}
+      </Script>
       <body>
         <Header />
         <main>{children}</main>
